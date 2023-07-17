d8c4be1ddc2a158ae227cf87bb3a3dec2570c4f2
dilanSachi <dilansachinthanos@gmail.com>
2023-07-17 13:29:12 +0530

    Add bouncycastle and crypto dependency

--- native/src/main/java/module-info.java
+++ native/src/main/java/module-info.java
@@ -1,4 +1,7 @@
-module io.ballerina.stdlib.kafka.runtime {
+module io.xlibb.mqtt {
     requires io.ballerina.runtime;
-    requires org.eclipse.paho.client.mqttv3;
+    requires io.ballerina.stdlib.crypto;
+    requires org.eclipse.paho.mqttv5.client;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.pkix;
 }
